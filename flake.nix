9de4c15c7f76714a94361822d4c284f555dbefb0
achillesgh <achillesgh@proton.me>
2026-07-03 21:59:52 +0530

    change:make home manager service for user run as user service.

--- flake.nix
+++ flake.nix
@@ -71,6 +71,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { inherit inputs mkNixPak; };
+            home-manager.startAsUserService = true;
             home-manager.sharedModules = [ ];
             home-manager.users.achilles = ./hm;
             home-manager.users.confman = ./manager;
