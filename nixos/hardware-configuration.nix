ea4e8f859eaf0f31d87a06427e3ed04fe15eda74
achillesgh <achillesgh@proton.me>
2026-07-03 21:54:05 +0530

    change:nvidia drivers to proprietary
    enable:power managment in nvidia
    
    remove:explicit package settings for nvidia

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -82,14 +82,15 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    open = true;
+    open = false;
+    powerManagement.enable = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #    package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       offload = {
         enable = true;
