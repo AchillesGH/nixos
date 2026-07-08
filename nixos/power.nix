472a79dc365340bd4ef1a48fabb382840b076a9d
achillesgh <achillesgh@proton.me>
2026-07-08 11:25:03 +0530

    comment out charge thresholds in tlp config since they are useless to me

--- nixos/power.nix
+++ nixos/power.nix
@@ -17,9 +17,8 @@
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 80;
 
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      # START_CHARGE_THRESH_BAT0 = 40;
+      # STOP_CHARGE_THRESH_BAT0 = 80;
 
       INTEL_GPU_POWER_PROFILE_ON_AC = "base";
       INTEL_GPU_POWER_PROFILE_ON_BAT = "power_saving";
