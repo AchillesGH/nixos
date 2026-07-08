6ea1b827953e21e9a24d276d8897c64e38fdc8d1
achillesgh <achillesgh@proton.me>
2026-07-08 12:39:52 +0530

    remove x11.enable comment

--- hm/default.nix
+++ hm/default.nix
@@ -289,7 +289,6 @@
   services.kdeconnect.indicator = true;
   home.pointerCursor = {
     gtk.enable = true;
-    # x11.enable = true;
     package = pkgs.apple-cursor;
     name = "macOS";
     size = 22;
