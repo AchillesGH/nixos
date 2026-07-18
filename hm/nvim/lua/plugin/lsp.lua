7a55ca26c77acad53788c3c4cc2d04887c280f40
achillesgh <achillesgh@proton.me>
2026-07-18 20:19:14 +0530

    nvim(plugin):enable blink.cmp with defaults, nvim(lsp):add more lsps

--- hm/nvim/lua/plugin/lsp.lua
+++ hm/nvim/lua/plugin/lsp.lua
@@ -1,5 +1,10 @@
+local cmp = require("blink.cmp")
+cmp.setup()
 vim.schedule(function()
-	  vim.lsp.enable({
-		  "pyright"
+	vim.lsp.enable({
+		"pyright",
+		"clangd",
+		"rust-analyzer",
+		"asm-lsp"
 	})
 end)
