e0eaee170cfdc33c2cab133dc76991d54919d825
achillesgh <achillesgh@proton.me>
2026-07-18 20:19:54 +0530

    nvim(plugin):add nvim-tree

--- hm/nvim/lua/plugin/nvim-tree.lua
+++ hm/nvim/lua/plugin/nvim-tree.lua
@@ -1,27 +1,19 @@
 vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
-  -- optionally enable 24-bit colour
-  vim.opt.termguicolors = true
-
-  -- empty setup using defaults
-  require("nvim-tree").setup()
-
-  -- OR setup with a config
-
-  ---@type nvim_tree.config
-  local config = {
-    sort = {
-      sorter = "case_sensitive",
-    },
-    view = {
-      width = 30,
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = true,
-    },
-  }
-  require("nvim-tree").setup(config)
-
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+local config = {
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+}
+require("nvim-tree").setup(config)
+vim.keymap.set('n', '<C-S-E>', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle nvim-tree' })
