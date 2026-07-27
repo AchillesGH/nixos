33ecf551254a0a7b18e21c65f6a370630f5accc1
achillesgh <achillesgh@proton.me>
2026-07-27 18:44:57 +0530

    disable `systemd-pcrlogin@{,1002}` due to errors (non-blocking) during boot after update

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -12,4 +12,12 @@
   };
   boot.loader.timeout = 0;
 
+  # Temporary patch for error (graceful, non-blocking) during boot
+  # until I figure out what triggered it.
+  # Context for future: Happend after update, possible causes might
+  # be lanzaboote update measured boot, still unlikely. Systemd version
+  # did NOT change across the update. LUKS tpm-backed unlocking still
+  # works flawlessly.
+  systemd.services."systemd-pcrlogin@".enable = false;
+  systemd.services."systemd-pcrlogin@1002".enable = false;
 }
