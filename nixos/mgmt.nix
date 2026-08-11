1e25eda69fcada675464c680a63319dcc38f81ac
achillesgh <achillesgh@proton.me>
2026-08-11 20:05:37 +0530

    partial work at ditching confman

--- nixos/mgmt.nix
+++ nixos/mgmt.nix
@@ -4,13 +4,13 @@ let
   sysrbs = pkgs.writeShellApplication {
     name = "sysrbs-cmd";
     runtimeInputs = [ pkgs.nixos-rebuild ];
-    text = "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake /home/confman/system#nixos";
+    text = "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake /home/achilles/nixos#nixos";
   };
 
   sysrbb = pkgs.writeShellApplication {
     name = "sysrbb-cmd";
     runtimeInputs = [ pkgs.nixos-rebuild ];
-    text = "${pkgs.nixos-rebuild}/bin/nixos-rebuild boot --flake /home/confman/system#nixos";
+    text = "${pkgs.nixos-rebuild}/bin/nixos-rebuild boot --flake /home/achilles/nixos#nixos";
   };
 
 in
