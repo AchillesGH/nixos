d9bae25866d8f8486e679ea807ff4dfe85cbe816
achillesgh <achillesgh@proton.me>
2026-09-02 18:59:36 +0530

    pin lanzaeboote to v1.1.0

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
